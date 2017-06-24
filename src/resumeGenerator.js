f1cad4ac446e1f1ddfb997d63678a9befe2dcacf
Sean Goedecke <seangoedecke@gmail.com>
2017-06-24 14:24:33 +1000

    Remove duplicates and improve tagline

--- src/resumeGenerator.js
+++ src/resumeGenerator.js
@@ -1,23 +1,17 @@
 import faker from 'faker';
 import sample from 'lodash/sample';
+import times from 'lodash/times';
+import remove from 'lodash/remove';
 
 const generateResume = () => ({
   name: faker.fake("{{name.firstName}} {{name.lastName}}"),
-  tagline: `${tagLineFlair()} ${faker.company.catchPhrase().toLowerCase()}`,
+  tagline: `${tagLineFlair()} ${faker.hacker.ingverb()} ${faker.hacker.adjective()} ${faker.hacker.noun()}s`,
   email: faker.internet.email().toLowerCase(),
   jobs: [
     generateJob(), generateJob()
   ],
-  skills: [
-    skill(),
-    skill(),
-    skill(),
-    skill(),
-    skill()
-  ],
-  interests: [
-    hobby(), hobby(), hobby()
-  ],
+  skills:  selectMultiple(getSkills, 4),
+  interests: selectMultiple(getHobbies, 5),
   degrees: [
     {
       type: `${degreeType()} in ${degreeTopic()}`,
@@ -26,6 +20,9 @@ const generateResume = () => ({
     }
   ]
 })
+
+export default generateResume;
+
 
 const degreeType = () => (
   sample([
@@ -78,19 +75,6 @@ const jobVerb = () => (
   ])
 )
 
-const skill = () => (
-  sample([
-    faker.hacker.abbreviation(),
-    faker.hacker.abbreviation() + '.js',
-    'Cloud ' + faker.hacker.abbreviation(),
-    'React',
-    'Ruby on Rails',
-    'Django',
-    'Docker',
-    'Kubernetes'
-  ])
-)
-
 const jobNoun = () => (
   sample([
     'Growth',
@@ -102,19 +86,40 @@ const jobNoun = () => (
   ])
 )
 
-const hobby = () => (
-  sample([
-    'hiking',
-    'coffee',
-    'reading',
-    'swimming',
-    'triathlons',
-    'hackathons',
-    'craft beer',
-    'local theatre',
-    'volunteering at soup kitchens',
-    'patting dogs'
-  ])
-)
+// ensure we don't have duplicates in lists where that would look bad
+// (e.g skills, hobbies)
+const selectMultiple = (getList, num) => {
+  const result = []
+  let tempList = getList()
+  times(num, () => {
+    let tempResult = sample(tempList)
+    remove(tempList, (res) => (res == tempResult))
+    result.push(tempResult)
+  })
+  return result
+}
 
-export default generateResume;
+
+const getHobbies = () => ([
+  'hiking',
+  'coffee',
+  'reading',
+  'swimming',
+  'triathlons',
+  'hackathons',
+  'craft beer',
+  'local theatre',
+  'volunteering at soup kitchens',
+  'patting dogs'
+])
+
+const getSkills = () => ([
+  faker.hacker.abbreviation(),
+  faker.hacker.abbreviation() + '.js',
+  'Cloud ' + faker.hacker.abbreviation(),
+  'React',
+  'Ruby on Rails',
+  'Django',
+  'Docker',
+  'Kubernetes'
+])
